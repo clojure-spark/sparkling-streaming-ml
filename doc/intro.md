44b82b52133e66d384d597473a87b3c2577ee1bd
Steve Chan <chanshunli@gmail.com>
2017-08-18 17:48:01 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sparkling-16-ml
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sparkling_16_ml/core_test.clj
@@ -0,0 +1,7 @@
+(ns sparkling-16-ml.core-test
+  (:require [clojure.test :refer :all]
+            [sparkling-16-ml.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
