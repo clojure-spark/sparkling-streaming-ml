aa9499cca912a4d74a06e50f9df59572428a8420
Steve Chan <chanshunli@gmail.com>
2017-08-18 18:28:09 +0800

    Sparkling 1.2.3 + Spark 2.10 ~ 1.6.0

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [gorillalabs/sparkling "2.1.2"]
+                 [gorillalabs/sparkling "1.2.3"]
                  [org.apache.spark/spark-mllib_2.10 "1.6.0"]]
-  ;;:aot [#".*" sparkling.serialization sparkling.destructuring]
+  :aot [#".*" sparkling.serialization sparkling.destructuring sparkling-16-ml.core]
   :main sparkling-16-ml.core
   :profiles {:provided {:dependencies [[org.apache.spark/spark-core_2.10 "1.6.0"]]}
              :dev {:plugins [[lein-dotenv "RELEASE"]]}}
--- src/sparkling_16_ml/core.clj
+++ src/sparkling_16_ml/core.clj
@@ -1,2 +1,12 @@
 (ns sparkling-16-ml.core
-  (:import (sparkinterface VectorClojure)))
+  (:gen-class)
+  (:require
+   [sparkling.conf :as conf]
+   [sparkling.core :as spark]
+   [sparkling.scalaInterop :as scala])
+  (:import
+   (org.apache.spark.api.java JavaRDD)
+   (sparkinterface VectorClojure)
+   (org.apache.spark.mllib.linalg Vectors)
+   (org.apache.spark.mllib.regression StreamingLinearRegressionWithSGD LabeledPoint)
+   (org.apache.spark.streaming Seconds StreamingContext)))
